37fde3d8f6b5f62cc91e01927cfd184e61c1b551
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-08-05 00:43:08 +0530

    adds domain names, incorrect costs

--- domains.js
+++ domains.js
@@ -7,4 +7,28 @@ const domainsList = [
     site: "https://howmanyfavoursdoesmattowecrystal.com/",
     cost: 9,
   },
+  {
+    site: "https://crystalfan.club",
+    cost: 9,
+  },
+  {
+    site: "http://elliefan.club/",
+    cost: 9,
+  },
+  {
+    site: "https://matthewwang.me/",
+    cost: 9,
+  },
+  {
+    site: "http://mattxw.com//",
+    cost: 9,
+  },
+  {
+    site: "https://whatthefatdog.com/",
+    cost: 9,
+  },
+  {
+    site: "http://theanniejumpedoverthemoon.com",
+    cost: 9,
+  },
 ];
