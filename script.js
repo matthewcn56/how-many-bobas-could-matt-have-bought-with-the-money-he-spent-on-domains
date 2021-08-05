cb7346f086701c36d430d76f5cbcc305406cf1ac
Matthew Nieva <matthewcn56@gmail.com>
2021-08-04 23:40:30 -0700

    boba customization set-up

--- script.js
+++ script.js
@@ -3,7 +3,64 @@
 //TODO: use user location to calculate local tax and display local currency
 
 const BOBA_COST = 5; // assuming $5 bobas
-const GEOCODING_API_KEY = 'AIzaSyBIfXhMxXV5ZX8Jd0Gbiq59mEZyR0A8pXk'; // owned by dtjanaka
+const GEOCODING_API_KEY = "AIzaSyBIfXhMxXV5ZX8Jd0Gbiq59mEZyR0A8pXk"; // owned by dtjanaka
+
+const urlQueryString = window.location.search;
+const urlParams = new URLSearchParams(urlQueryString);
+
+//using t4's menu: https://t4togo.com/Menu1
+
+const sizes = {
+  SMALL: "small",
+  LARGE: "large",
+};
+
+const SMALL_PRICE = 0;
+const UPCHARGE_PRICE = 0.5;
+
+const flavors = {
+  WINTERMELON: {
+    NAME: "wintermelon milk tea",
+    PRICE: 4.55,
+  },
+  OKINAWA: {
+    NAME: "okinawa milk tea",
+    PRICE: 4.8,
+  },
+  HOKKAIDO: {
+    NAME: "royal hokkaido milk tea",
+    PRICE: 4.5,
+  },
+};
+
+const toppings = {
+  BOBA: {
+    NAME: "boba pearls",
+    PRICE: 0.5,
+  },
+  CHEESE_FOAM: {
+    NAME: "cheese foam",
+    PRICE: 1.5,
+  },
+};
+
+let flavorParam = urlParams.get("flavor");
+flavorParam ??= "WINTERMELON";
+const flavor = flavors[flavorParam].NAME;
+const flavorPrice = flavors[flavorParam].PRICE;
+
+const sizeParam = urlParams.get("size");
+const size = sizeParam ? sizes[sizeParam] : "small";
+let sizePrice = SMALL_PRICE;
+sizePrice += size === "large" ? UPCHARGE_PRICE : 0;
+
+const topping1Param = urlParams.get("topping1");
+const topping1 = topping1Param ? toppings[topping1Param].NAME : "none";
+const topping1Price = topping1 !== "none" ? toppings[topping1Param].PRICE : 0;
+
+const topping2Param = urlParams.get("topping2");
+const topping2 = topping2Param ? toppings[topping2Param].NAME : "none";
+const topping2Price = topping2 !== "none" ? toppings[topping2Param].PRICE : 0;
 
 /**
  * @param {array of domain objects} domains
@@ -15,10 +72,21 @@ function calcDomainsCost(domains) {
 
 /**
  * @param {float} cost
+ * @param {float} flavorPrice
+ * @param {float} sizePrice
+ * @param {float} topping1Price
+ * @param {float} topping2Price
  * @returns number of bobas that could be purchased
  */
-function costToBobas(cost) {
-  return cost / BOBA_COST;
+function costToBobas(
+  cost,
+  flavorPrice,
+  sizePrice,
+  topping1Price,
+  topping2Price
+) {
+  const bobaPrice = flavorPrice + sizePrice + topping1Price + topping2Price;
+  return cost / bobaPrice;
 }
 
 /**
@@ -26,41 +94,62 @@ function costToBobas(cost) {
  * @returns header for total cost
  */
 function generateTotalCost(domains) {
-  const totalCostText = document.createElement('h2');
-  totalCostText.id = 'total-cost-header';
+  const totalCostText = document.createElement("h2");
+  totalCostText.id = "total-cost-header";
   const totalCost = calcDomainsCost(domains);
   totalCostText.innerHTML =
     'Enough is enough. Matt spends<br/><span id="cost">$' +
     totalCost +
-    '</span><br/>on domains every year. ' +
+    "</span><br/>on domains every year. " +
     'That\'s enough to buy<br/><span id="total-num-bobas">' +
-    costToBobas(totalCost).toFixed(1) +
-    '</span><br/>bobas.';
+    costToBobas(
+      calcDomainsCost(domains),
+      flavorPrice,
+      sizePrice,
+      topping1Price,
+      topping2Price
+    ).toFixed(1) +
+    "</span><br/>";
+  totalCostText.innerHTML += size + " bobas";
+  if (topping1 != "none") totalCostText.innerHTML += " with " + topping1;
+  if (topping2 != "none") totalCostText.innerHTML += " and " + topping2;
   return totalCostText;
 }
+/**
+ * @param {string} size
+ * @param {string} topping1
+ * @param {string} topping2
+ * @returns string describing the boba
+ */
 
 /**
  * @param {domain object} domain
  * @returns div for a card
  */
 function generateCardFromObject(domain) {
-  const cardContainer = document.createElement('div');
-  const cardSiteName = document.createElement('h3');
-  const cardBobaCount = document.createElement('h2');
+  const cardContainer = document.createElement("div");
+  const cardSiteName = document.createElement("h3");
+  const cardBobaCount = document.createElement("h2");
 
-  cardContainer.className = 'card-container';
+  cardContainer.className = "card-container";
 
-  const numBobas = costToBobas(domain.cost).toFixed(1);
+  const numBobas = costToBobas(
+    domain.cost,
+    flavorPrice,
+    sizePrice,
+    topping1Price,
+    topping2Price
+  ).toFixed(1);
 
   cardSiteName.innerHTML =
-    'Instead of buying <a href="' + domain.site + '">' + domain.site + '</a>,';
-  cardSiteName.className = 'card-site-name';
+    'Instead of buying <a href="' + domain.site + '">' + domain.site + "</a>,";
+  cardSiteName.className = "card-site-name";
   cardBobaCount.innerHTML =
-    'Matt could have bought ' +
+    "Matt could have bought " +
     numBobas +
-    (numBobas === 1 ? ' boba' : ' bobas') +
-    ' this year.';
-  cardBobaCount.className = 'card-boba-count';
+    (numBobas === 1 ? " boba" : " bobas") +
+    " this year.";
+  cardBobaCount.className = "card-boba-count";
 
   cardContainer.appendChild(cardSiteName);
   cardContainer.appendChild(cardBobaCount);
@@ -73,8 +162,8 @@ function generateCardFromObject(domain) {
  * @returns div with all cards
  */
 function generateCards(domains) {
-  const cards = document.createElement('div');
-  cards.id = 'cards';
+  const cards = document.createElement("div");
+  cards.id = "cards";
 
   for (let i = 0; i < domains.length; i++) {
     cards.appendChild(generateCardFromObject(domains[i]));
@@ -91,19 +180,33 @@ let state;
  * @param {array of domain objects} domains
  */
 function onloadPopulate(domains) {
-  if('geolocation' in navigator) {
+  console.log(
+    "size is: " +
+      size +
+      ", topping 1 is: " +
+      topping1 +
+      ", topping 2 is: " +
+      topping2
+  );
+  if ("geolocation" in navigator) {
     // geolocation is available
     navigator.geolocation.getCurrentPosition(handlePosition);
   }
-  document.getElementById('total-cost').appendChild(generateTotalCost(domains));
-  document.getElementById('cost-breakdown').appendChild(generateCards(domains));
+  document.getElementById("total-cost").appendChild(generateTotalCost(domains));
+  document.getElementById("cost-breakdown").appendChild(generateCards(domains));
 }
 
 async function handlePosition(position) {
   const lat = position.coords.latitude;
   const long = position.coords.longitude;
 
-  const url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long + '&key=' + GEOCODING_API_KEY;
+  const url =
+    "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+    lat +
+    "," +
+    long +
+    "&key=" +
+    GEOCODING_API_KEY;
   const geocodingResponse = await fetch(url);
   const geocodingResult = await geocodingResponse.json();
   console.log(geocodingResult);
